803e0c5bbb3a011d3b5b8d147abcc8655eb8548d
biruk <birukanley2022@gmail.com>
2022-04-15 11:22:06 +0300

    Adding A style for gallery

---
+++ js/style.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 5,
+    slideShadows: true,
+  },
+  loop: true,
+});
+
+function click() {
+  document.getElementById('')
+}
+
+function city() {
+  
+  
+}
+function campus() {
+  
+}
