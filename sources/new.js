065eb4cd46023e4b5d71b44a0de8d0f0e780f2f2
Biruk Anley <birukanley2022@gmail.com>
2023-03-15 22:24:10 +0300

    update js

---
+++ sources/new.js
@@ -0,0 +1,30 @@
+var images = [
+    "image1.jpg",
+    "image2.jpg",
+    "image3.jpg",
+    "image4.jpg",
+    "image5.jpg"
+  ];
+  var currentImageIndex = 0;
+  var previousButton = document.getElementById("previous");
+  var nextButton = document.getElementById("next");
+  
+  previousButton.addEventListener("click", showPreviousImage);
+  nextButton.addEventListener("click", showNextImage);
+  function showPreviousImage() {
+    currentImageIndex--;
+    if (currentImageIndex < 0) {
+      currentImageIndex = images.length - 1;
+    }
+    document.getElementById("image").src = images[currentImageIndex];
+  }
+  
+  function showNextImage() {
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+      currentImageIndex = 0;
+    }
+    document.getElementById("image").src = images[currentImageIndex];
+  }
+  showNextImage();
+    
